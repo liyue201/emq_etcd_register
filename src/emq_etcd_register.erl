a3a61c376bbc4dbe97f702a2e809456555d56d67
stirlingx <stirlingx@git.coding.net>
2016-11-28 17:03:16 +0800

    update

--- src/emq_etcd_register.erl
+++ src/emq_etcd_register.erl
@@ -2,13 +2,9 @@
 
 -export([start_link/0]).
 -export([loop/4]).
--export([looop/0]).
 
 start_link() ->
     io:format("start_link()~n"),
-
-    %ok,Pid = spawn_link(?MODULE, loop, []),
-    %{ok, Pid}.
 
     {ok, Url} = application:get_env(emq_etcd_register, etcd_url),
     io:format("etcd_url = ~p~n", [Url]),
@@ -26,27 +22,7 @@ start_link() ->
     {ok, Pid}.
 
 
-looop() ->
-
-    io:format("looop()~n"),
-
-    TTL = 5,
-    Sttl = integer_to_list(TTL),
-    Url = "http://120.24.44.201:4001",
-    Key = "/emq/node1",
-    Value = "tcp://127.0.0.1:1883",
-
-    httpc:request(put, {Url ++ "/v2/keys" ++ Key, [], ["application/x-www-form-urlencoded"],
-            "value=" ++ Value ++ "&ttl=" ++ Sttl}, [], [], default),
-
-    timer:sleep(trunc(TTL * 1000 / 3)),
-
-    looop().
-
-
 loop(Url, Key, Value, TTL) ->
-
-    io:format("loop()~n"),
 
     %httpc:request(put, {Url, [], ["application/x-www-form-urlencoded"], "value=127.0.0.1:5555&ttl=10"}, [], [], default),
 
